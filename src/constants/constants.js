ccbc33f1f5a4b303fb385dc5b6830d51f2dfe07b
Suman <suman@itapgroup.com>
2023-10-28 11:22:43 +0530

    first commit

---
+++ src/constants/constants.js
@@ -0,0 +1,6 @@
+export const LANGUAGES = [
+    { label: "English", code: "en" },
+    { label: "Spanish", code: "es" },
+    { label: "Italian", code: "it" },
+    { label: "Hindi", code: "hi" },
+  ];
